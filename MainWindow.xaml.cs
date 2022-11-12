2df4be985b90c35f495df131dc993afc8e9f12ec
Needlide <max.litvinyuck553@gmail.com>
2022-11-12 15:29:31 +0200

    Add project files.

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace VideoPlayerWPF
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        #region Fields
+        private Player _player;
+        #endregion
+
+        #region Constructor
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+        #endregion
+
+        #region Button click
+        private void PlayButton_Click(object sender, RoutedEventArgs e)
+        {
+            PlayButton.Visibility = Visibility.Collapsed;
+            PauseButton.Visibility = Visibility.Visible;
+            _player.Play();
+        }
+
+        private void PauseButton_Click(object sender, RoutedEventArgs e)
+        {
+            PlayButton.Visibility = Visibility.Visible;
+            PauseButton.Visibility = Visibility.Collapsed;
+            _player.Pause();
+        }
+
+        private void PreviousButton_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void NextButton_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void MediaOpenBlock_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            _player = new Player(this);
+            OpenMediaWindow openMediaWindow = new OpenMediaWindow(_player);
+            openMediaWindow.Show();
+        }
+        #endregion
+    }
+}
---
+++ OpenMediaWindow.xaml.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace VideoPlayerWPF
+{
+    /// <summary>
+    /// Interaction logic for OpenMediaWindow.xaml
+    /// </summary>
+    public partial class OpenMediaWindow : Window
+    {
+        #region Fields
+        public Uri MediaUri { get; set; }
+        private Player _player;
+        #endregion
+
+        #region Constructors
+        public OpenMediaWindow()
+        {
+            InitializeComponent();
+        }
+
+        internal OpenMediaWindow(Player player)
+        {
+            InitializeComponent();
+            _player = player;
+        }
+        #endregion
+
+        #region Button click
+        private void FileDialogButton_Click(object sender, RoutedEventArgs e)
+        {
+            _player.PlayVideoFromFile();
+            Close();
+        }
+
+        private void OkResultButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (!string.IsNullOrEmpty(UrlBox.Text))
+            {
+                try
+                {
+                    MediaUri = new Uri(UrlBox.Text);
+                    Close();
+                    _player.PlayVideoFromUri(MediaUri);
+                }
+                catch (UriFormatException)
+                {
+                    System.Windows.MessageBox.Show("Enter a valid URL!", "Invalid URL", MessageBoxButton.OK, MessageBoxImage.Warning);
+                }
+            }
+        }
+
+        private void CancelResultButton_Click(object sender, RoutedEventArgs e) => Close();
+        #endregion
+    }
+}
